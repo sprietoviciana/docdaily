b8cf0454325482b02f88d8cd06b9a2768e884f09
Silvia Prieto <info@silviaprieto.com>
2025-04-25 12:29:13 +0200

    Create database and added in folder 'Database'

---
+++ back/Database/tables.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE docdaily;
+
+USE docdaily;
+
+CREATE TABLE
+    patients (
+        id INT auto_increment primary key,
+        name VARCHAR(50) not null,
+        lastname VARCHAR(50) not null,
+    );
+
+CREATE TABLE
+    doctors (
+        id INT auto_increment primary key,
+        name VARCHAR(50) not null,
+        lastname VARCHAR(50) not null
+    );
+
+CREATE TABLE
+    agenda (
+        id INT auto_increment primary key,
+        date DATE,
+        start_time TIME,
+        end_time TIME,
+        treatment TEXT,
+        doctor_id INT,
+        patient_id INT,
+        FOREIGN KEY (doctor_id) REFERENCES doctors (id),
+        FOREIGN KEY (patient_id) REFERENCES patients (id)
+    );
